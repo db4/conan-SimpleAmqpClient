812922ddb95f7feb0d89d038b43cca32df01bace
Dmitry Bely <d.bely@recognize.ru>
2018-10-03 11:11:41 +0300

    Test hangs with  MTd MSVC runtime (exclude it from CI build for now)

--- build.py
+++ build.py
@@ -3,7 +3,6 @@ import platform
 
 if __name__ == "__main__":
     builder = ConanMultiPackager(build_policy="missing")
-    builder.visual_runtimes = ["MD", "MDd"]
     shared_option = False if platform.system() == "Windows" else None
     builder.add_common_builds(shared_option_name=shared_option, pure_c=False)
     builder.run()
